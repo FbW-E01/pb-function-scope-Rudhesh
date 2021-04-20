8ccdeb80d1e273dd5568f25ac168bbb1259f305f
dci <rudeshdhote@gmail.com>
2021-04-20 13:48:44 +0200

    initial commit

---
+++ scope.js
@@ -0,0 +1,41 @@
+function expo(i, n) {
+
+
+    for (n = 1; n <= 5; n++) {
+            console.log(Math.pow(i, n));
+
+    }
+
+}
+
+expo(3, 5);
+
+
+
+function expo2(x,y) {
+    for (let y = 1; y <= 8; y++) {
+        console.log(Math.pow(x,y));
+        
+    }
+}
+expo2(2,8);
+
+let fruit = "mango"; 
+function printFavoriteFruit(x) {
+    fruit = "mango";
+    console.log(`My favorite fruit is: ${fruit}`)
+}
+
+printFavoriteFruit(fruit)
+
+
+
+function exponent(4,5) {
+
+   for (let i = 0; i < 5; i++) {
+       
+
+       
+   } 
+    
+}
